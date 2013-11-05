e75a70199dc702b2ea484e4d82aaf40178952771
Maxime Bedard <maxim3.bedard@gmail.com>
2013-11-05 14:56:37 -0500

    Modification de la fabrique pour inclure la création du jeu

--- src/log121/lab3/api/FabriquePartie.java
+++ src/log121/lab3/api/FabriquePartie.java
@@ -1,15 +1,18 @@
 package log121.lab3.api;
 
 public abstract class FabriquePartie{
-	
-	public abstract CollectionJoueur creerJoueurs();	
-	public abstract CollectionDes creerDes();
-	
-	public final Jeu creerJeu(int nombreTours, IStrategie strategie){
-		Jeu jeu = new Jeu(nombreTours, strategie);
-		jeu.setListeJoueurs(creerJoueurs());
+		
+	public Jeu construire()
+	{
+		Jeu jeu = creerJeu();
 		jeu.setListeDes(creerDes());
-		return null;
+		jeu.setListeJoueurs(creerJoueurs());
+		return jeu;
 	}
+	
+		
+	protected abstract CollectionJoueur creerJoueurs();	
+	protected abstract CollectionDes creerDes();
+	protected abstract Jeu creerJeu();
 
 }
--- src/log121/lab3/bunco/Bunco.java
+++ src/log121/lab3/bunco/Bunco.java
@@ -3,19 +3,29 @@ package log121.lab3.bunco;
 import log121.lab3.api.CollectionDes;
 import log121.lab3.api.CollectionJoueur;
 import log121.lab3.api.FabriquePartie;
+import log121.lab3.api.Jeu;
+import log121.lab3.api.Joueur;
 
 public class Bunco extends FabriquePartie{
 
-
-	public CollectionJoueur creerJoueurs() {
-		
+	@Override
+	protected CollectionJoueur creerJoueurs() {
+		// TODO Auto-generated method stub
 		return null;
 	}
 
-	
-	public CollectionDes creerDes() {
-		
+	@Override
+	protected CollectionDes creerDes() {
+		// TODO Auto-generated method stub
 		return null;
-	}	
+	}
+
+	@Override
+	protected Jeu creerJeu() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
 
 }
